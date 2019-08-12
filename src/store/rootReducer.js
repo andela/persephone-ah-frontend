2307c53b3e7a1e50444c5758ac066023953d7bce
Damilola Adekoya <dharmykoya38@gmail.com>
2019-08-12 11:32:12 +0100

    feature(comment-article):allow user comment on article To allow users comment on an article To write test [#166816068]

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -3,12 +3,14 @@ import themeToggler from '../components/Header/index.reducer';
 import authReducer from '../views/SignupPage/signup.reducer';
 import loginReducer from '../views/LoginPage/login.reducer';
 import singleArticleReducer from '../views/ReadArticlePage/readArticle.reducer';
+import commentReducer from '../components/CreateComment/createComment.reducer';
 
 const rootReducer = combineReducers({
   theme: themeToggler,
   signup: authReducer,
   user: loginReducer,
-  readArticle: singleArticleReducer
+  readArticle: singleArticleReducer,
+  articleComment: commentReducer
 });
 
 export default rootReducer;
